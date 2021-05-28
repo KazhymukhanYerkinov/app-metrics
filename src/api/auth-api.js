b94592c6921b00b39d688484cc2e09a4363b784c
KazhymukhanYerkinov <61079138+KazhymukhanYerkinov@users.noreply.github.com>
2021-05-28 15:22:49 +0600

    done

--- src/api/auth-api.js
+++ src/api/auth-api.js
@@ -1,6 +1,8 @@
 import { instance } from "./api"
 
 export const authAPI = {
+
+  
   login(email, password) {
     return instance.post('auth/login/', { email, password }).then(response => response.data);
   }
