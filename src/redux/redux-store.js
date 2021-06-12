398c8c2417c4023f6f4649b1d353d05e6e34a670
Dice <dicenameismy@gmail.com>
2021-06-12 16:11:21 +0600

    add old klik reducer

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -2,10 +2,12 @@ import thunkMiddlewere from 'redux-thunk';
 import { applyMiddleware, combineReducers, createStore } from 'redux';
 import authReducer from './auth-reducer';
 import appReducer from './app-reducer';
+import oldKlikReducer from './old-klik-reducer'
 
 let rootReducer = combineReducers({
   auth: authReducer,
   app: appReducer,
+  oldKlik: oldKlikReducer
 });
 
 let store = createStore(rootReducer, applyMiddleware(thunkMiddlewere));
