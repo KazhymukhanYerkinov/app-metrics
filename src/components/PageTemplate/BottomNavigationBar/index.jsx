9c78f855f4f383545d34841bb41e8e3f07798098
[Yernar] <toktaryernar@gmail.com>
2021-06-24 11:58:21 +0600

    bottom nav done

--- src/components/PageTemplate/BottomNavigationBar/index.jsx
+++ src/components/PageTemplate/BottomNavigationBar/index.jsx
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from "react";
-import { useHistory } from "react-router-dom";
+import { NavLink, useHistory } from "react-router-dom";
 
+import shortid from "shortid";
+
+import { sidebar as sidebarItems } from "@data/sidebar";
 import SelectSite from "../Navbar/SelectSite";
 import Notification from "../Navbar/Notification";
 import UserActions from "../Navbar/UserActions";
 
 import "./index.less";
 
-export default function BottomNavigationBar() {
+export default function BottomNavigationBar({ viewMode }) {
   const history = useHistory();
   const [
     bottomNavListCollapsed,
@@ -66,7 +69,57 @@ export default function BottomNavigationBar() {
             : ""
         }`}
       >
-        <li className="list-item"></li>
+        <li className="list-item">
+          <div
+            className={`actions__switch-mode-btn actions__switch-mode-btn--${viewMode}`}
+          >
+            <div className="switch-mode-btn__ball"></div>
+          </div>
+        </li>
+
+        <li className="list-item">
+          <ul className="sidebar__list">
+            {sidebarItems.map((el) => (
+              <li
+                key={shortid.generate()}
+                className="list-item"
+                onClick={() => history.push(el.url)}
+              >
+                {el.image}
+                <NavLink
+                  className="list-item__text"
+                  to={el.url}
+                >
+                  {el.name}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </li>
+
+        <div className="list-item">
+          <div className="sidebar__plan-box">
+            <ul className="plan-box__list">
+              <li className="list-item">
+                <h3 className="list-item__title">
+                  Credits
+                </h3>
+                <p className="list-item__text">993</p>
+              </li>
+              <li className="list-item">
+                <h3 className="list-item__title">Plan</h3>
+                <p className="list-item__text">Free</p>
+              </li>
+              <li className="list-item">
+                <h3 className="list-item__title">Next</h3>
+                <p className="list-item__text">7 days</p>
+              </li>
+            </ul>
+            <div className="plan-box__upgrade-btn">
+              Upgrade plan
+            </div>
+          </div>
+        </div>
       </ul>
     </footer>
   );
